8a933030406b315052719041aa2d677285463091
SystemRoott <System_Root@outlook.com.tr>
2017-12-08 22:18:46 +0300

    Try Update

--- Fatura Kayıt Sistemi/Formlar/Modul.Designer.cs
+++ Fatura Kayıt Sistemi/Formlar/Modul.Designer.cs
@@ -78,7 +78,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(604, 12);
+            this.button1.Location = new System.Drawing.Point(568, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
